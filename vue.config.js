1fced84e9a0a4ddd22a90d10b3a3ac484a2949e9
way-up <376634272@qq.com>
2019-10-09 14:50:25 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,12 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-<<<<<<< HEAD
-        target: "http://front.run4wd.com/",
-        ws: true,
-        changeOrigin: true
-=======
         target: "http://dfa34h.natappfree.cc/",
         ws: true,
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/', // rewrite path
         }
->>>>>>> update
       },
       "^/foo": {
         target: "<other_url>"
